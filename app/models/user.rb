fe232c3f8268584c10f2f6c54d42839dd0d60460
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-01-20 19:03:57 +0900

    本名、フリガナに正規表現でバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  with_options presence: true, format: { with: /\A[ぁーンァ-ヶ一-龥々]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :first_name_kana, presence: true
+    validates :last_name_kana, presence: true
+  end
   validates :birth_day, presence: true
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
