b676af211ef40ed658e8a4dca56aa99e3d89d6db
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-01-20 17:17:01 +0900

    パスワードにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
   validates :birth_day, presence: true
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
