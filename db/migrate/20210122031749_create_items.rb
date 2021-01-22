6c9d5f196e84a4f692ecf79038f11b088fc0863b
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-01-22 12:41:57 +0900

    itemsテーブル作成

--- db/migrate/20210122031749_create_items.rb
+++ db/migrate/20210122031749_create_items.rb
@@ -1,7 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :item_name, null: false
+      t.text :explanation, null: false
+      t.integer :category_id, null: false
+      t.integer :state_id, null: false
+      t.integer :burden_id, null: false
+      t.integer :prefectures_id, null: false
+      t.integer :days_id, null: false
+      t.integer :price, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_031705) do
+ActiveRecord::Schema.define(version: 2021_01_22_031749) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "explanation", null: false
+    t.integer "category_id", null: false
+    t.integer "state_id", null: false
+    t.integer "burden_id", null: false
+    t.integer "prefectures_id", null: false
+    t.integer "days_id", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
