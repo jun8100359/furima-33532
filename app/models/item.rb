5a14c616f8e4d636048d500b1b7d2e141a6c4a28
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-01-25 09:33:47 +0900

    値段のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,6 @@ class Item < ApplicationRecord
   belongs_to :choice
   validates :item_name, :explanation, :category_id, :state_id, :burden_id, :prefectures_id, :days_id, :price, presence: true
   validates :category_id, :state_id, :burden_id, :prefectures_id, :days_id, numericality: { other_than: 1 }
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: {with: /\A[0-9]+\z/ }
   has_one_attached :image
 end
