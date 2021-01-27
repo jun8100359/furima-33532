f984a2a1f71e665b0b8f366046f54875a0f1a6a2
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-01-27 18:39:14 +0900

    ログイン状態の出品者以外が編集ページへ移動しようとすると、トップページへ遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :move_to_index, except: [:index, :show]
+  
   def index
     @items = Item.order('created_at DESC')
   end
@@ -26,6 +28,12 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
@@ -33,5 +41,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :explanation, :category_id, :state_id, :burden_id, :prefecture_id,
                                  :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
