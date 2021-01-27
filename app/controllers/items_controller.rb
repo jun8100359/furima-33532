250de20a535a49580ac0b645090ad72ed0093939
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-01-27 18:43:28 +0900

    ログアウト状態のユーザーがURLを直接入力して編集ページへ移動しようとすると、ログインページへ遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, except: [:index, :show]
-  
+
   def index
     @items = Item.order('created_at DESC')
   end
