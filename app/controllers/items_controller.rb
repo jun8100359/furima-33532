51e206f5896e5ce7840104b6c53dbe48703b734c
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-01-27 20:33:24 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, except: [:index, :show]
   before_action :move_to_index2, only: :edit
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -51,5 +49,9 @@ class ItemsController < ApplicationController
   def move_to_index2
     @item = Item.find(params[:id])
     return redirect_to root_path unless current_user.id == @item.user.id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
