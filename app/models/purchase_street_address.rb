ca439e9e8707458f12946e38c3dd2d90b34dd84e
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-01-30 16:40:29 +0900

    モデル編集

--- app/models/purchase_street_address.rb
+++ app/models/purchase_street_address.rb
@@ -1,2 +1,15 @@
-class PurchaseStreetAddress < ApplicationRecord
+class PurchaseStreetAddress
+    include ActiveModel::Model
+    attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :address, :building, :phone_number, :purchase
+
+    validates :user_id, :item_id, presence: true
+    validates :postal_code, :prefecture_id, :municipalities, :address, :phone_number, :purchase_id, presence: true
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :phone_number, format: { with: /\A\d{10}\z/ }, numericality: { less_than: 11 }
+
+    def save
+      purchase = Purchase.create(user_id: item.user_id, item_id: item.id)
+      StreetAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
+    end
 end
--- app/models/street_address.rb
+++ app/models/street_address.rb
@@ -1,7 +1,4 @@
 class StreetAddress < ApplicationRecord
   belongs_to :purchase
-  validates :postal_code, :prefecture_id, :municipalities, :address, :phone_number, :purchase_id, presence: true
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-  validates :phone_number, format: { with: /\A\d{10}\z/ }, numericality: { less_than: 11 }
+
 end
