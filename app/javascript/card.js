839f75e6c8fe8cd49849087fc26f4b383e72fcb9
jun8100359 <76466071+jun8100359@users.noreply.github.com>
2021-01-31 15:08:08 +0900

    クレジット決済機能のクライアントサイド実装(クレジットカード情報のトークン化)

---
+++ app/javascript/card.js
@@ -0,0 +1,26 @@
+const pay = () => {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_street_address[number]"),
+      cvc: formData.get("purchase_street_address[cvc]"),
+      exp_month: formData.get("purchase_street_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_street_address[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
